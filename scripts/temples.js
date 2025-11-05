c58eb995d4c16ed397f07539b9c30c287b9cf233
josevegalafuente <jvegalafuente@byupathway.edu>
2025-11-05 14:52:38 -0400

    First Release - Corrections Made

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,7 +3,7 @@ if (currentYearSpan) {
   currentYearSpan.textContent = new Date().getFullYear();
 }
 
-const lastModifiedParagraph = document.getElementById('lastModified');
-if (lastModifiedParagraph) {
-  lastModifiedParagraph.textContent = `Last Modification: ${document.lastModified}`;
+const lastModifiedSpan = document.getElementById('lastModified');
+if (lastModifiedSpan) {
+  lastModifiedSpan.textContent = document.lastModified;
 }
